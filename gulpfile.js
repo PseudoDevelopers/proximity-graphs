81ddae088c1c45d0ed9b06498c79454ce72c5ce6
PSEUDO <Pseudo.Developers@gmail.ccom>
2020-09-14 22:37:00 +0500

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+const fileinclude = require('gulp-file-include')
+const htmlmin = require('gulp-htmlmin')
+
+const paths = {
+    html: './html',
+    indexFile: './html/index.html',
+    dest: './'
+}
+
+async function updateIndexFile() {
+    return gulp.src([paths.indexFile])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        // .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(paths.dest))
+}
+
+gulp.watch(paths.html, updateIndexFile)
+
+
+exports.default = updateIndexFile
