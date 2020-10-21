c45ebab65a9bf0aa462fafe2ea0f923c23d13a3b
PSEUDO <Pseudo.Developers@gmail.ccom>
2020-10-21 11:54:37 +0500

    components deleted from root

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a gravity simulator
+Newton laws motion & universal gravitation are used to simulate the object motion.
--- assets/scripts/functions.js
+++
@@ -1,7 +0,0 @@
-function lerp(min, max, val) {
-    return min + (max - min) * val
-}
-
-function rescale(min, max, val) {
-    return (val - min) / (max - min)
-}
